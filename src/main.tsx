01cdd2950fcdaf035fff9166bed61aef24fe6e9a
Manos Markou <markou_13@yahoo.com>
2026-04-17 12:09:42 +0300

    Provider to handle dark - light  theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './theme/ThemeContext.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +18,10 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
